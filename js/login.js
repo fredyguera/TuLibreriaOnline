3a3f7a35e2a3d29bfb725a2fc2123952b584e602
fredyguera <freddguerra@gmail.com>
2025-05-12 20:12:43 -0600

    Primera carga de proyecto venProductos

---
+++ js/login.js
@@ -0,0 +1,14 @@
+document.getElementById('login-form') addEventListener('submit', function(e) {
+    e.preventDefault();//Evitar que el formulario se envie automaticamente al servidor
+
+    const usuario = document.getElementById ('usuario').value; 
+    const pass = document.getElementById ('pass').value;
+
+   //SIMULACION básica de login
+    if(usuario === 'admin' && pass === '12345'){
+        window.location.href = '../private/dashboard.html';
+    }
+    else{
+        document.getElementById ('mensajelogin').textContent = "Usuario o contraseña incorrectos"
+    }
+});
